7f0a670a5faceb2e63b53749f987ad831e23f649
AnonymousKnight <tnizar019@gmail.com>
2021-05-28 22:21:31 +0100

    Added the possibility to wether choose to show the similar results or no

--- file_finder.py
+++ file_finder.py
@@ -5,6 +5,7 @@ from match import res_perc
 target = input("Name of the file you are looking for: ")
 print("What is the type of the file?\n1.Text\n2.Video\n3.Audio\n4.Readme\n5.Any")
 file_type = input("(1/2/3/4/5)?: ")
+add_similar = input("Do you want to add similar files in the search results?\n(Y/N): ")
 #path = input("Directory to look in: ")
 
 extensions = {
@@ -31,7 +32,7 @@ def search(file_name):
                 print(rf'[INFO]Go to: {root}\{str(file)}')
                 result.append(rf"{root}\{str(file)}")
             #similarity depending on the percentage of the match.py module and if the two files' names are the same    
-            elif res_perc(list(file_name), list(file))>=50.0 and (list(file)[0] == list(file)[0]):
+            elif add_similar == 'Y' and res_perc(list(file_name), list(file))>=50.0 and (list(file)[0] == list(file)[0]):
                 print(rf'[INFO]Similar file at: {root}\{str(file)}')
                 result.append(rf"{root}\{str(file)}")
             else:
